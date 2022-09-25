543fc48b64686a01bbef5ed1bb1eac473eb2570f
ju-bezdek <juraj.bezdek@gmail.com>
2022-09-25 13:57:41 +0200

    version increase

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 
 setuptools.setup(name='labelatorio',
-                version='0.2.0',
+                version='0.2.1',
                 description='Labelator.io python client',
                 long_description=open('README.md').read(),
                 long_description_content_type='text/markdown',
