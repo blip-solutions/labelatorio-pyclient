a6ab3006fa51df69549b03571ed8a633576f4479
ju-bezdek <juraj.bezdek@gmail.com>
2023-01-01 22:49:19 +0100

    v.0.2.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 
 setuptools.setup(name='labelatorio',
-                version='0.2.3',
+                version='0.2.4',
                 description='Labelator.io python client',
                 long_description=open('README.md').read(),
                 long_description_content_type='text/markdown',
